44ca42147eb003c361498f3b913ea32149b26b9d
RajikuBlanc <rajiku.blanc@gmail.com>
2021-05-24 12:48:01 +0900

    header menu comp

--- src/components/NavMenu.jsx
+++ src/components/NavMenu.jsx
@@ -1,9 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import styled from "styled-components";
-import { MdMenu } from "react-icons/md";
-import { MdClose } from "react-icons/md";
+import { MdMenu, MdClose } from "react-icons/md";
 
+const NavMenu = () => {
+  const [showNav, setShowNav] = useState(false);
+  return (
+    <NavMenuStyles>
+      {/* Humberger Menu */}
+      <div
+        className="mobile-menu-icon"
+        onClick={() => setShowNav(!showNav)}
+        role="button"
+        onKeyDown={() => setShowNav(!showNav)}
+        tabIndex={0}
+      >
+        <MdMenu />
+      </div>
+
+      <ul className={!showNav ? "navItems hide-navItems" : "navItems"}>
+        {/* Close Button */}
+        <div
+          className="closeNavIcon"
+          onClick={() => setShowNav(!showNav)}
+          role="button"
+          onKeyDown={() => setShowNav(!showNav)}
+          tabIndex={0}
+        >
+          <MdClose />
+        </div>
+
+        <li>
+          <NavLink to="/" exact>
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/about">About</NavLink>
+        </li>
+        <li>
+          <NavLink to="/projects">Projects</NavLink>
+        </li>
+        <li>
+          <NavLink to="/contact">Contact</NavLink>
+        </li>
+      </ul>
+    </NavMenuStyles>
+  );
+};
+
+// Styles
 const NavMenuStyles = styled.div`
   position: fixed;
   top: 0;
@@ -36,33 +82,52 @@ const NavMenuStyles = styled.div`
       color: var(--white);
     }
   }
-`;
+  .closeNavIcon {
+    display: none;
+  }
 
-const NavMenu = () => {
-  return (
-    <NavMenuStyles>
-      <div className="mobile-menu-icon">
-        <MdMenu />
-      </div>
-      <ul>
-        <MdClose />
-        <li>
-          <NavLink to="/" exact>
-            Home
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/about">About</NavLink>
-        </li>
-        <li>
-          <NavLink to="/projects">Projects</NavLink>
-        </li>
-        <li>
-          <NavLink to="/contact">Contact</NavLink>
-        </li>
-      </ul>
-    </NavMenuStyles>
-  );
-};
+  .mobile-menu-icon {
+    display: none;
+    position: absolute;
+    right: 1rem;
+    top: 1rem;
+    width: 40px;
+    cursor: pointer;
+    outline: none;
+  }
+  @media only screen and (max-width: 768px) {
+    .hide-navItems {
+      transform: translateY(calc(-100% - var(--top)));
+    }
+    .mobile-menu-icon {
+      display: block;
+    }
+    .navItems {
+      --top: 1rem;
+      transition: 0.3s ease transform;
+      background-color: var(--deep-dark);
+      padding: 2rem;
+      width: 90%;
+      max-width: 300px;
+      border-radius: 12px;
+      position: absolute;
+      top: var(--top);
+      right: 1rem;
+      .closeNavIcon {
+        display: block;
+        width: 30px;
+        margin: 0 0 0 auto;
+        cursor: pointer;
+        * {
+          pointer-events: none;
+        }
+      }
+      li {
+        display: block;
+        margin-bottom: 1rem;
+      }
+    }
+  }
+`;
 
 export default NavMenu;
