5134c56bdba91834c6d30ccd51a7548bfafecc7c
RajikuBlanc <rajiku.blanc@gmail.com>
2021-06-14 00:16:03 +0900

    projects Page

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,9 +1,78 @@
-import React from "react";
+import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
+import SectionTitle from '../components/SectionTitle';
+import { MdSearch } from 'react-icons/md';
+import ProjectItem from '../components/ProjectItem';
+import ProjectInfo from '../assets/data/projects';
+import { Medias } from '../styles/Media';
+const ProjectsStyle = styled.div`
+  padding: 10rem 0;
+  .projects__list {
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+    gap: 5rem;
+    margin-top: 5rem;
+  }
+  .projects__searchbar {
+    position: relative;
+    width: 300px;
+    margin-top: 5rem;
+  }
+  .projects__searchbar input {
+    width: 100%;
+    font-size: 2rem;
+    padding: 0.8rem;
+    border-radius: 6px;
+    color: var(--black);
+    outline: none;
+  }
+  .projects__searchbar .searchIcon {
+    position: absolute;
+    width: 20px;
+    right: 10px;
+  }
+  .projects__searchbar .searchIcon path {
+    color: var(--deep-dark);
+  }
+  ${Medias.tab} {
+    .projects__searchbar,
+    .projects__searchbar form,
+    .projects__searchbar input {
+      width: 100%;
+    }
+  }
+`;
 
 export default function Projects() {
+  const [searchText, setSearchText] = useState('');
+  const [projectsData, setProjectsData] = useState(ProjectInfo);
+
+  useEffect(() => {
+    if (searchText === '') return;
+    setProjectsData(() => ProjectInfo.filter(item => item.name.toLowerCase().match(searchText.toLowerCase())));
+  }, [searchText]);
+
+  function handleChange(e) {
+    e.preventDefault();
+    setSearchText(e.target.value);
+  }
+
   return (
-    <div>
-      <h1>This is Projects Page</h1>
-    </div>
+    <ProjectsStyle>
+      <div className="container">
+        <SectionTitle subTitle="some of my recent works" Title="Projects" />
+        <div className="projects__searchbar">
+          <form>
+            <input type="text" value={searchText} onChange={handleChange} placeholder="Project Name" />
+            <MdSearch className="searchIcon" />
+          </form>
+        </div>
+        <div className="projects__list">
+          {ProjectInfo.map(item => (
+            <ProjectItem img={item.img} title={item.name} desc={item.desc} key={item.id} />
+          ))}
+        </div>
+      </div>
+    </ProjectsStyle>
   );
 }
